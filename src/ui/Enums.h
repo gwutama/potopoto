bb7b6e36a3615d6ac637a3497ecc7a06e014637e
Galuh Utama <galuh.utama@gwutama.de>
2024-09-21 22:43:21 +0200

    Cleaned up obsolete files (mostly imgui/implot related) since we are using wxwidgets

--- src/ui/Enums.h
+++
@@ -1,6 +0,0 @@
-#ifndef POTOPOTO_ENUMS_H
-#define POTOPOTO_ENUMS_H
-
-enum class ActiveTool { Zoom, Hand };
-
-#endif //POTOPOTO_ENUMS_H
--- src/ui/UiUtils.cpp
+++
@@ -1,5 +0,0 @@
-#include "UiUtils.h"
-
-cv::Point UiUtils::ImVec2ToCvPoint(const ImVec2& in_point) {
-    return {static_cast<int>(in_point.x), static_cast<int>(in_point.y)};
-}
